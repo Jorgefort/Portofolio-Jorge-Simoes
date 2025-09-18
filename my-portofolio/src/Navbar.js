4b2861b6caf50f6923228a04e2de3291a2c9258a
jorge <102530@glr.nl>
2025-09-18 12:52:31 +0000

    error fixed?

--- my-portofolio/src/Navbar.js
+++ my-portofolio/src/Navbar.js
@@ -4,9 +4,9 @@ function Navbar() {
   return (
     <header className="App-header">
       <nav>
-        <a href='#'><img src={logoImg} alt="Logo" className="nav-logo" /></a>
+        <a href="#hero"><img src={logoImg} alt="Logo" className="nav-logo" /></a>
         <ul className='nav-links'>
-          <li><a href="#hero">About</a></li>
+          <li><a href="#about">About</a></li>
           <li><a href="#projects">Projects</a></li>
           <li><a href="#favprojects">Contact</a></li>
         </ul>
