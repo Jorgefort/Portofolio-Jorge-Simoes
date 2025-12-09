4ca6000c54d3a8745efd2fa7d49a91fa77693f8d
jorge <102530@glr.nl>
2025-12-09 21:40:24 +0000

    imgaes are in divs now

--- my-portofolio/src/CircularNav.js
+++ my-portofolio/src/CircularNav.js
@@ -28,13 +28,13 @@ const CircularNav = ({ onNavigate, activePage }) => {
           
           <a href="#projects" className="nav-link" onClick={(e) => handleNavClick(e, 'projects')}>
             <text 
-              fill={activePage === 'projects' ? "#ffffff" : "#B4AC97"} 
+              fill="#B4AC97" 
               fontSize="30" 
               fontWeight={activePage === 'projects' ? "700" : "400"} 
               letterSpacing="2px" 
               fontFamily="'Zen Old Mincho', serif" 
               textAnchor="middle"
-              style={{ filter: activePage === 'projects' ? 'drop-shadow(0 0 2px rgba(255,255,255,0.5))' : 'none' }}
+              style={{ filter: activePage === 'projects' ? 'drop-shadow(0 0 5px rgba(180, 172, 151, 0.9))' : 'none' }}
             >
               <textPath href="#circlePath" startOffset="17%">
                 projects |
@@ -44,13 +44,13 @@ const CircularNav = ({ onNavigate, activePage }) => {
 
           <a href="#about" className="nav-link" onClick={(e) => handleNavClick(e, 'about')}>
             <text 
-              fill={activePage === 'about' ? "#ffffff" : "#B4AC97"} 
+              fill="#B4AC97" 
               fontSize="30" 
               fontWeight={activePage === 'about' ? "700" : "400"} 
               letterSpacing="2px" 
               fontFamily="'Zen Old Mincho', serif" 
               textAnchor="middle"
-              style={{ filter: activePage === 'about' ? 'drop-shadow(0 0 2px rgba(255,255,255,0.5))' : 'none' }}
+              style={{ filter: activePage === 'about' ? 'drop-shadow(0 0 5px rgba(180, 172, 151, 0.9))' : 'none' }}
             >
               <textPath href="#circlePath" startOffset="50%">
                 about |
@@ -60,13 +60,13 @@ const CircularNav = ({ onNavigate, activePage }) => {
 
           <a href="#contact" className="nav-link" onClick={(e) => handleNavClick(e, 'contact')}>
             <text 
-              fill={activePage === 'contact' ? "#ffffff" : "#B4AC97"} 
+              fill="#B4AC97" 
               fontSize="30" 
               fontWeight={activePage === 'contact' ? "700" : "400"} 
               letterSpacing="2px" 
               fontFamily="'Zen Old Mincho', serif" 
               textAnchor="middle"
-              style={{ filter: activePage === 'contact' ? 'drop-shadow(0 0 2px rgba(255,255,255,0.5))' : 'none' }}
+              style={{ filter: activePage === 'contact' ? 'drop-shadow(0 0 5px rgba(180, 172, 151, 0.9))' : 'none' }}
             >
               <textPath href="#circlePath" startOffset="83%">
                 contact |
